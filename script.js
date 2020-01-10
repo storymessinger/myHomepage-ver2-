3e92653e4ab66d69f70843d7479bdd428e144af2
Kwang Eun Park <paukparliament@gmail.com>
2020-01-10 21:52:40 +0900

    added placeholder videos

--- script.js
+++ script.js
@@ -10,17 +10,23 @@ var textbox = Renderer.createTextbox({
   text: 'HAPPY\nBIRTHDAY'
 });
 
+// var canvas = document.createElement('canvas');
+// canvas.width = 300;
+// canvas.height = 300;
+// var ctx = canvas.getContext('2d');
 
 
-setTimeout(function(){
-  addVid('jundong');
-}, 500);
+for (var i=0; i<8; i++) {
+  addVid('jundong-comp_'+i);
+}
+
+
 
 
 function addVid (name) {
   var vid = document.createElement('video');
   vid.setAttribute('src', '/videos/'+name+'.mp4');
-  vid.setAttribute('height', 250);
+  vid.setAttribute('height', 300);
   vid.setAttribute('autoplay', true);
   vid.setAttribute('webkit-playsinline', true);
   vid.setAttribute('playsinlinee', true);
@@ -33,16 +39,34 @@ function addVid (name) {
 
 
   vidBox.addEventListener('mouseenter', function(){
+    // attach video
+    // console.log(vidBox._time);
+    // if (vidBox._time) vid.currentTime = vidBox._time;
     vid.play();
   });
+
   vidBox.addEventListener('mouseleave', function(){
+    // vidBox._time = vid.currentTime;
     vid.pause();
+
+    // ctx.drawImage(vid, 0, 0, canvas.width, canvas.height);
+    // var dataURI = canvas.toDataURL('image/jpeg');
+    // vidBox.removeChild(vid);
+
+    // var img = document.createElement('img');
+    // img.setAttribute('src', dataURI);
+    // vidBox.appendChild(img);
   })
 
+  vidBox.addEventListener('click', function(){
+    vid.load();
+  })
+
+  var maxY = window.innerHeight - 300;
+  var maxX = window.innerWidth - 300;
+  vidBox.setAttribute('style', 'left: '+maxX*Math.random()+'px; top: '+maxY*Math.random()+'px;'); 
 
   document.querySelector('.cards-container').appendChild(vidBox);
-
-  console.log(displace);
 
   displace(vidBox, {
     constrain: true,
@@ -55,6 +79,7 @@ function addVid (name) {
       textboxList: [textbox],
       fontList: [font],
     },
-    textureUrl: '/font-textures'
+    textureUrl: '/font-textures',
+    pixelRatio: 0.8
   });
 }
